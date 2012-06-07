cd388dfbee490bc78f9dd5353a7f9cf997367ae6
Brandon Foltz <bsfoltzguitar@gmail.com>
2012-06-07 15:48:40 -0400

    Fixed support for custom menu(s).

--- functions.php
+++ functions.php
@@ -19,4 +19,9 @@ add_filter('excerpt_more', 'new_excerpt_more');
 if ( function_exists('register_sidebar') )
     register_sidebar();
 
+/* Register custom header menu. */    
+register_nav_menus( array(
+	'primary' => 'Primary Navigation'
+) );
+
 ?>
